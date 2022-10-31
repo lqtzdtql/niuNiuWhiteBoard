f3fd5cb751c6e49af95ba0cde4ccf002ffa510cf
GrongYH <952286890@qq.com>
2022-11-01 00:41:12 +0800

    feat: 添加了Room模型，并对后端代码重构

--- backend/niuNiuWhiteBoardBackend/models/users.go
+++ backend/niuNiuWhiteBoardBackend/models/users.go
@@ -1,15 +1,25 @@
 package models
 
+import (
+	"niuNiuWhiteBoardBackend/config"
+)
+
+const (
+	UserStateOnline  = "online"
+	UserStateOffline = "offline"
+)
+
 type Users struct {
-	Id          int64  `json:"id" xorm:"pk autoincr comment('主键') BIGINT(20)"`
+	ID          int64  `json:"id"  xorm:"pk autoincr comment('主键') BIGINT(20)"`
+	UUID        string `json:"uuid" xorm:"not null unique 'uuid' comment('用户唯一标识符') index VARCHAR(128)"`
 	Name        string `json:"name" xorm:"not null default '' comment('用户名') VARCHAR(50)"`
 	Mobile      string `json:"mobile" xorm:"not null default '' comment('手机号') VARCHAR(20)"`
 	Passwd      string `json:"passwd" xorm:"not null comment('密码') VARCHAR(50)"`
-	CreatedTime int64  `json:"created_time" xorm:"not null default 0 comment('创建时间') index INT(10)"`
-	UpdatedTime int64  `json:"updated_time" xorm:"not null default 0 comment('修改时间') index INT(10)"`
+	CreatedTime int64  `json:"created_time" xorm:"not null default 0 comment('创建时间') INT(10)"`
+	UpdatedTime int64  `json:"updated_time" xorm:"not null default 0 comment('修改时间') INT(10)"`
 }
 type UserRow struct {
-	Id     int64  `json:"id" xorm:"pk autoincr comment('主键') BIGINT(20)"`
+	ID     int64  `json:"id" xorm:"pk autoincr comment('主键') BIGINT(20)"`
 	Name   string `json:"name" xorm:"not null default '' comment('用户名') VARCHAR(50)"`
 	Mobile string `json:"mobile" xorm:"not null default '' comment('手机号') VARCHAR(20)"`
 }
@@ -17,7 +27,7 @@ type UserRow struct {
 var usersTable = "users"
 
 func (u *Users) GetRow() bool {
-	has, err := mEngine.Get(u)
+	has, err := conf.MEngine.Get(u)
 	if err == nil && has {
 		return true
 	}
@@ -25,12 +35,12 @@ func (u *Users) GetRow() bool {
 }
 func (u *Users) GetAll() ([]Users, error) {
 	var users []Users
-	err := mEngine.Find(&users)
+	err := conf.MEngine.Find(&users)
 	return users, err
 }
 
 func (u *Users) Add(trace *Trace, device *Device) (int64, error) {
-	session := mEngine.NewSession()
+	session := conf.MEngine.NewSession()
 	defer session.Close()
 	// add Begin() before any action
 	if err := session.Begin(); err != nil {
@@ -41,24 +51,25 @@ func (u *Users) Add(trace *Trace, device *Device) (int64, error) {
 		return 0, err
 	}
 
-	trace.Uid = u.Id
+	trace.Uid = u.ID
 	_, err = session.Insert(trace)
 	if err != nil {
 		return 0, err
 	}
-	device.Uid = u.Id
+	device.Uid = u.ID
 	_, err = session.Insert(device)
 	if err != nil {
 		return 0, err
 	}
-	return u.Id, session.Commit()
+	return u.ID, session.Commit()
 }
 func IsExistsMobile(mobile string) bool {
 	model := Users{Mobile: mobile}
 	return model.GetRow()
 }
+
 func (u *Users) GetRowById() (UserRow, error) {
 	var userRow UserRow
-	_, err := mEngine.Table(usersTable).Where("id=?", u.Id).Get(&userRow)
+	_, err := conf.MEngine.Table(usersTable).Where("id=?", u.ID).Get(&userRow)
 	return userRow, err
 }
